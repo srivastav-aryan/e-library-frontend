bbb583ce319f6db39f363949cfcd103b0cab7ad3
aryan srivastava <srivastavaryan03@gmail.com>
2025-06-28 17:10:47 +0530

    add more  responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
 
   return (
         <header
-      className="flex fixed z-40 w-full right-0 top-0 items-center justify-between border-b bg-white px-4 py-2 shadow-md transition-all duration-300 ease-in-out sm:px-6"
+      className="flex fixed z-40 right-0 top-0 items-center justify-between border-b bg-white px-4 py-2 shadow-md transition-all duration-300 ease-in-out sm:px-6"
       style={{ left: !isMobile && state == "expanded" ? "16rem" : "0" }}
     >
       <div className="flex items-center gap-2 sm:gap-4">
